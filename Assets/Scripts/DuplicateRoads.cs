f02a633ba816251f75f4640489c2984db1d38bdf
Jerry <jerryszy95@gmail.com>
2019-04-15 13:48:01 -0700

    smooth the road

--- Assets/Scripts/DuplicateRoads.cs
+++ Assets/Scripts/DuplicateRoads.cs
@@ -7,10 +7,11 @@ public class DuplicateRoads : MonoBehaviour
     public GameObject roadPrefab;
     int cnt = 1;
     float AbsAngle = 360;
+    float RoadLength = 2f;
     Vector3 AbsLocation = new Vector3(0,0,0);
-    int[] DistanceArray = new int[29] { 10, 20, 90, 20, 250, 190, 200, 250, 190, 200,
+    int[] DistanceArray = new int[29] { 10, 2, 2, 2, 2, 2, 2, 2, 200, 200,
      250, 190, 200, 250, 190, 200, 250, 190, 200, 250, 190, 200, 250, 190, 200, 250, 190, 200, 750};
-    int[] DegreeArray = new int[29] { 360, 15, 360, -5, 15, 360, -5, 15, 360, -5, 15, 360, -5, 15, 360, -5,
+    int[] DegreeArray = new int[29] { 360, 30/4, 30/4*2, 30/4*3, 30/4*4, 30/4*3, 30/4*2, 30/4, 360, -5, 15, 360, -5, 15, 360, -5,
      15, 360, -5, 15, 360, -5, 15, 360, -5, 15, 360, -5, 360};
     // Start is called before the first frame update
     void Start()
@@ -39,12 +40,12 @@ public class DuplicateRoads : MonoBehaviour
     {
         float slope = 2f * Mathf.PI / (float)(360 / angle);
         float AbsSlope;
-        for (int i = 0; i < length / 10; i++)
+        for (int i = 0; i < length / RoadLength; i++)
         {
             AbsSlope = 2f * Mathf.PI / (float)(360 / (90-AbsAngle));
             GameObject obj = GameObject.Instantiate(roadPrefab);
-            AbsLocation = AbsLocation + new Vector3(0, 1 / 2f * 10f * (Mathf.Sin(slope) + Mathf.Cos(AbsSlope)),
-                1 / 2f * 10f * (Mathf.Cos(slope) + Mathf.Sin(AbsSlope)));
+            AbsLocation = AbsLocation + new Vector3(0, 1 / 2f * RoadLength * (Mathf.Sin(slope) + Mathf.Cos(AbsSlope)),
+                1 / 2f * RoadLength * (Mathf.Cos(slope) + Mathf.Sin(AbsSlope)));
             obj.transform.position =  AbsLocation;
             obj.transform.Rotate(-1f * angle, 0, 0);
             AbsAngle = angle;
