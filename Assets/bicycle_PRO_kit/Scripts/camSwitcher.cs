4c60f388e73f13258e94936f62ef8a8f0cc39501
Bowen <zbw14@outlook.com>
2019-04-15 01:09:56 -0700

    add first view to bike

--- Assets/bicycle_PRO_kit/Scripts/camSwitcher.cs
+++ Assets/bicycle_PRO_kit/Scripts/camSwitcher.cs
@@ -1,4 +1,4 @@
-﻿// Writen by Boris Chuprin smokerr@mail.ru
+// Writen by Boris Chuprin smokerr@mail.ru
 using UnityEngine;
 using System.Collections;
 
@@ -9,6 +9,7 @@ public class camSwitcher : MonoBehaviour
 	public Camera aroundCamera;
 	public Transform cameraTarget;
 	private Camera currentCamera;
+    public Camera firstViewCamera;
 	//////////////////// for back Camera 
 	float dist = 3.0f;
 	float height = 1.0f;
@@ -48,12 +49,14 @@ public class camSwitcher : MonoBehaviour
 		backCamera.enabled = true;
 		aroundCamera.enabled = false;
 		currentCamera = backCamera;
+        firstViewCamera.enabled = false;
 		
 		if (GetComponent<Rigidbody> ()) GetComponent<Rigidbody> ().freezeRotation = true;
 	
 		currentTargetAngle = cameraTarget.transform.eulerAngles.z;
 	}
-	
+
+    bool firstView = false;
 	// Update is called once per frame
 	void LateUpdate ()
 	{
@@ -87,57 +90,87 @@ public class camSwitcher : MonoBehaviour
 
 		} else {
 #endif
-			backCamera.enabled = true;
-			aroundCamera.enabled = false;
-			backCamera.gameObject.SetActive (true);
-			aroundCamera.gameObject.SetActive (false);
-			currentCamera = backCamera;
-			
-			//////////////////// code for back Camera
-			backCamera.fieldOfView = backCamera.fieldOfView + outsideControls.Vertical * 20f * Time.deltaTime;
-			if (backCamera.fieldOfView > 85) {
-				backCamera.fieldOfView = 85;
-			}
-			if (backCamera.fieldOfView < 50) {
-				backCamera.fieldOfView = 50;
-			}
-			if (backCamera.fieldOfView < 60) {
-				backCamera.fieldOfView = backCamera.fieldOfView += 10f * Time.deltaTime;
-			}
-			if (backCamera.fieldOfView > 60) {
-				backCamera.fieldOfView = backCamera.fieldOfView -= 10f * Time.deltaTime;
-			}
-			
-			float wantedRotationAngle = cameraTarget.eulerAngles.y;
-			float wantedHeight = cameraTarget.position.y + height;
-			float currentRotationAngle = currentCamera.transform.eulerAngles.y;
-			float currentHeight = currentCamera.transform.position.y;
-			
-			currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, 3 * Time.deltaTime);
-			currentHeight = Mathf.Lerp (currentHeight, wantedHeight, 2 * Time.deltaTime);
-			
-			Quaternion currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
-			currentCamera.transform.position = cameraTarget.position;
-			currentCamera.transform.position -= currentRotation * Vector3.forward * dist;
-			currentCamera.transform.position = new Vector3 (currentCamera.transform.position.x, currentHeight, currentCamera.transform.position.z);
-			currentCamera.transform.LookAt (cameraTarget);
+            if (Input.GetKeyUp(KeyCode.F1))
+                firstView = true;
 
-			//New camera features.
-			//Now camera leaning with biker, so horizon is not always horizontal :)
-			//If you don't like it, just disable
-			//from this -----------------------------------------------------------------------
+            if (Input.GetKeyDown(KeyCode.F2))
+                firstView = false;
 
-			// rotate camera according with bike leaning
-			if (cameraTarget.transform.eulerAngles.z >0 && cameraTarget.transform.eulerAngles.z < 180) {
-				currentTargetAngle = cameraTarget.transform.eulerAngles.z/10;
-			}
-			if (cameraTarget.transform.eulerAngles.z >180){
-				currentTargetAngle = -(360-cameraTarget.transform.eulerAngles.z)/10;
-			}
-			currentCamera.transform.rotation = Quaternion.Euler (height*10, currentRotationAngle, currentTargetAngle);
-			//to this -------------------------------------------------------------------------
-		#if UNITY_STANDALONE || UNITY_WEBPLAYER// turn camera rotaion ONLY for mobile for free touch screen anywhere
-		}
+            if (!firstView)
+            {
+                backCamera.enabled = true;
+                aroundCamera.enabled = false;
+                firstViewCamera.enabled = false;
+                backCamera.gameObject.SetActive(true);
+                aroundCamera.gameObject.SetActive(false);
+                firstViewCamera.gameObject.SetActive(false);
+                currentCamera = backCamera;
+
+                //////////////////// code for back Camera
+                backCamera.fieldOfView = backCamera.fieldOfView + outsideControls.Vertical * 20f * Time.deltaTime;
+                if (backCamera.fieldOfView > 85)
+                {
+                    backCamera.fieldOfView = 85;
+                }
+                if (backCamera.fieldOfView < 50)
+                {
+                    backCamera.fieldOfView = 50;
+                }
+                if (backCamera.fieldOfView < 60)
+                {
+                    backCamera.fieldOfView = backCamera.fieldOfView += 10f * Time.deltaTime;
+                }
+                if (backCamera.fieldOfView > 60)
+                {
+                    backCamera.fieldOfView = backCamera.fieldOfView -= 10f * Time.deltaTime;
+                }
+
+                float wantedRotationAngle = cameraTarget.eulerAngles.y;
+                float wantedHeight = cameraTarget.position.y + height;
+                float currentRotationAngle = currentCamera.transform.eulerAngles.y;
+                float currentHeight = currentCamera.transform.position.y;
+
+                currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, 3 * Time.deltaTime);
+                currentHeight = Mathf.Lerp(currentHeight, wantedHeight, 2 * Time.deltaTime);
+
+                Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+                currentCamera.transform.position = cameraTarget.position;
+                currentCamera.transform.position -= currentRotation * Vector3.forward * dist;
+                currentCamera.transform.position = new Vector3(currentCamera.transform.position.x, currentHeight, currentCamera.transform.position.z);
+                currentCamera.transform.LookAt(cameraTarget);
+
+                //New camera features.
+                //Now camera leaning with biker, so horizon is not always horizontal :)
+                //If you don't like it, just disable
+                //from this -----------------------------------------------------------------------
+
+                // rotate camera according with bike leaning
+                if (cameraTarget.transform.eulerAngles.z > 0 && cameraTarget.transform.eulerAngles.z < 180)
+                {
+                    currentTargetAngle = cameraTarget.transform.eulerAngles.z / 10;
+                }
+                if (cameraTarget.transform.eulerAngles.z > 180)
+                {
+                    currentTargetAngle = -(360 - cameraTarget.transform.eulerAngles.z) / 10;
+                }
+                currentCamera.transform.rotation = Quaternion.Euler(height * 10, currentRotationAngle, currentTargetAngle);
+                //to this -------------------------------------------------------------------------
+            }
+            else
+            {
+                backCamera.enabled = false;
+                aroundCamera.enabled = false;
+                firstViewCamera.enabled = true;
+                backCamera.gameObject.SetActive(false);
+                aroundCamera.gameObject.SetActive(false);
+                firstViewCamera.gameObject.SetActive(true);
+                currentCamera = firstViewCamera;
+
+                //firstViewCamera.transform.position = GameObject.Find("rigid_bike").transform.position +
+                  //                                  new Vector3(0, 0.8f, 0f);
+            }
+#if UNITY_STANDALONE || UNITY_WEBPLAYER// turn camera rotaion ONLY for mobile for free touch screen anywhere
+        }
 		#endif
 	}
 }
