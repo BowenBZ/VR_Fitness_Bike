aad5abc9ceefe81b391bdd4873130cf22c94461f
Jerry <jerryszy95@gmail.com>
2019-05-05 17:23:56 -0700

    add road sign

---
+++ Assets/KEI - Speed Signs/Resources/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public bool rot;
+
+	// Use this for initialization
+	void Start () {
+        rot = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (rot) { transform.RotateAround(Vector3.zero, Vector3.left, 20 * Time.deltaTime); }
+
+    }
+}
