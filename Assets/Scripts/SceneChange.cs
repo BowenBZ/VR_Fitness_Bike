ad2983bb92c2cfff5cc7eab749a3a8ade348d63c
Bowen <zbw14@outlook.com>
2019-05-15 15:38:32 -0700

    Finished Main Menu

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
