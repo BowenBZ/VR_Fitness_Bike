6eaed0a1bf037909ab73d2d5c9d28182c2b61c00
Bowen <zbw14@outlook.com>
2019-05-15 17:47:25 -0700

    Finish scene change

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -8,7 +8,7 @@ public class SceneChange : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadScene("Scenes/Main");
     }
 
 
